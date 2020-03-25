88543cd7e0b0e914c6017da9cb20059aa04f4e6e
Talal Waseem <60611063+talalwas@users.noreply.github.com>
2020-03-25 19:22:28 +0500

    s

---
+++ Lec 5/script.js
@@ -0,0 +1,15 @@
+console.log("External log");
+
+var x=3.1
+var y=2
+var z=2
+console.log(x+y);
+
+function sum(a,b,c){
+    return a+b+c
+}
+
+console.log(sum(2,y,z))
+
+var abc=sum
+console.log(abc)
