8a07c27bef3f6f4ef66c3023a1d3dca83d8b43b2
Talal Waseem <60611063+talalwas@users.noreply.github.com>
2020-03-26 13:53:00 +0500

    lec 6

---
+++ Lec 6/realScript.js
@@ -0,0 +1,7 @@
+function updat(){
+    console.log("check")
+    var input=document.getElementById("1")
+    var output=document.getElementById("output")
+    console.log(input.value)
+    output.innerHTML=input.value
+}
---
+++ Lec 6/script.js
@@ -0,0 +1,7 @@
+
+function onClick(){
+    console.log("Click hua")
+}
+function hover(){
+    console.log("before click")
+}
