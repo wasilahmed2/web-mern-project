95789dd455ce2d7084993a09a34fbd69cc3dc233
Talal Waseem <60611063+talalwas@users.noreply.github.com>
2020-08-11 17:25:23 +0500

    ll

--- RestfulAPIusingmongoexpressnode/restfulapi/app.js
+++ RestfulAPIusingmongoexpressnode/restfulapi/app.js
@@ -7,12 +7,17 @@ var mongoose=require("mongoose");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/api/users');
 var productsRouter = require('./routes/api/products');
+var prodRouter=require('./routes/products/products')
+var loginRouter=require('./routes/login/login')
 var config= require("config");
+const router = require('./routes/index');
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,9 +25,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
+app.use('/products',prodRouter);
 app.use('/api/products', productsRouter);
+app.use('/login', loginRouter)
 
 
 // catch 404 and forward to error handler
--- RestfulAPIusingmongoexpressnode/restfulapi/routes/login/login.js
+++ RestfulAPIusingmongoexpressnode/restfulapi/routes/login/login.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET home page. */
+/* GET products page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Express' });
  
 });
 
