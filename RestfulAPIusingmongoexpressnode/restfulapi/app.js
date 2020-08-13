21a2dd54c8629933d9667f9951fd2b3982b58e50
wasilahmed2 <68281960+wasilahmed2@users.noreply.github.com>
2020-08-14 04:34:46 +0500

    Login Authentication added
    authentication without MongoDB

--- RestfulAPIusingmongoexpressnode/restfulapi/app.js
+++ RestfulAPIusingmongoexpressnode/restfulapi/app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -11,7 +15,20 @@ var prodRouter = require("./routes/products/products");
 var loginRouter = require("./routes/login/login");
 var config = require("config");
 const router = require("./routes/index");
-var app = express();
+const app = express();
+const bcrypt = require("bcrypt");
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require("./passport-config");
+const passport = require("passport");
+initializePassport(passport, (email) =>
+  users.find((user) => user.email === email),
+  id => users.find(user => user.id === id)
+);
+
+const users = [];
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -20,10 +37,55 @@ app.set("view engine", "pug");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
+
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.pug");
+});
+
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.pug");
+});
+
+app.post("/login", checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.pug");
+});
+
+app.post("/register", checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch {
+    res.redirect("/register");
+  }
+});
+
 app.use("/api/users", usersRouter);
 app.use("/products", prodRouter);
 app.use("/api/products", productsRouter);
@@ -51,4 +113,25 @@ mongoose
   })
   .then(() => console.log("Connected to Mongo...."))
   .catch((error) => console.log(error.message));
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
 module.exports = app;
