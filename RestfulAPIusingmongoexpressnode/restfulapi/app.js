cfa73ba1c1b0fdb67da44a4424bd804394bfb4e1
wasilahmed2 <68281960+wasilahmed2@users.noreply.github.com>
2020-08-13 16:40:32 +0500

    Initialized

--- RestfulAPIusingmongoexpressnode/restfulapi/app.js
+++ RestfulAPIusingmongoexpressnode/restfulapi/app.js
@@ -1,58 +1,54 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose=require("mongoose");
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/api/users');
-var productsRouter = require('./routes/api/products');
-var prodRouter=require('./routes/products/products')
-var loginRouter=require('./routes/login/login')
-var config= require("config");
-const router = require('./routes/index');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/api/users");
+var productsRouter = require("./routes/api/products");
+var prodRouter = require("./routes/products/products");
+var loginRouter = require("./routes/login/login");
+var config = require("config");
+const router = require("./routes/index");
 var app = express();
 
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-
-
-app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
-app.use('/products',prodRouter);
-app.use('/api/products', productsRouter);
-app.use('/login', loginRouter)
-
+app.use("/", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/products", prodRouter);
+app.use("/api/products", productsRouter);
+app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message =  err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-mongoose.connect(config.get("db")
-,
-{ useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("Connected to Mongo...."))
-.catch((error) => console.log(error.message));
+mongoose
+  .connect(config.get("db"), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Mongo...."))
+  .catch((error) => console.log(error.message));
 module.exports = app;
